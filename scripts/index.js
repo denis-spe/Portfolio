34b9243ac5e10f710f1074d84dd06729c7a550dc
denisbrian07@gmail.com <denisbrian07@gmail.com>
2023-03-07 20:10:00 +0200

    Added aside tag

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+/* ==== Glory Be To God ===== */
+
+/* 
+ Handling aside buttons 
+ for opening and closing aside
+ menu
+*/
+// Initialize the openAsideId and closeAsideId
+const openAsideId = document.getElementById("open-aside-id");
+const closeAsideId = document.getElementById("close-aside-id");
+
+console.log(closeAsideId);
